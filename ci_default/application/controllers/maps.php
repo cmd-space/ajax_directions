33253ec292631e597f41ebc31f9f0808b88c80c6
Mason Crane <masonecrane@gmail.com>
2015-04-22 17:32:44 -0600

    finally was able to communicate with api and retrieve json data, and successfully display using ajax

--- ci_default/application/controllers/maps.php
+++ ci_default/application/controllers/maps.php
@@ -5,6 +5,13 @@ class Maps extends CI_Controller {
   {
   	$this->load->view('index');
   }
+
+  public function directions() {
+  	$from = str_replace(' ', '', $this->input->post('from'));
+  	$to = str_replace(' ', '', $this->input->post('to'));
+  	$html = file_get_contents('https://maps.googleapis.com/maps/api/directions/json?origin='.$from.'&destination='.$to.'&key=AIzaSyDFysk3DkUyMwzl-3bnmVnlKuUT-ytaXWU');
+  	$this->output->set_content_type('application/json')->set_output($html);
+  }
 }
 
 //end of main controller
--- ci_default/application/views/index.php
+++ ci_default/application/views/index.php
@@ -3,14 +3,42 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Googlemaps API</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script>
+		$(document).ready(function() {
+			$('form').submit(function() {
+				$('#directions').html('<img src="/assets/images/loading.gif">');
+				$.post($(this).attr('action'), $(this).serialize(), function(res) {
+					var htmlString = '';
+					if(res.routes.length !== 0) {
+						htmlString = '<h2>Directions from '+res.routes[0].legs[0].start_address+' to '+res.routes[0].legs[0].end_address+'</h2>';
+						for(var i = 0; i < res.routes[0].legs[0].steps.length; i++) {
+							htmlString += '<p>'+(i+1)+'. '+res.routes[0].legs[0].steps[i].html_instructions+'</p>';
+						}
+					}
+					else {
+						htmlString = 'Not Found';
+					}
+					$('#directions').html(htmlString);
+				}, 'json');
+				return false;
+			});
+		});
+	</script>
+	<style>
+		h1 {
+			display: inline-block;
+		}
+	</style>
 </head>
 <body>
 	<form action="/maps/directions" method="post">
-		<label for="from">From: </label>
+		<label for="from"><h1>From: </h1></label>
 		<input type="text" id="from" name="from">
-		<label for="to">To: </label>
+		<label for="to"><h1>To: </h1></label>
 		<input type="text" id="to" name="to">
 		<input type="submit" value="Get directions!">
 	</form>
+	<div id='directions'></div>
 </body>
 </html>
